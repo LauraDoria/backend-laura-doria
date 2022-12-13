d04452f6eafcde3be102f43d797f30f926baeb75
LauraDoria <94977527+LauraDoria@users.noreply.github.com>
2022-12-12 23:10:22 -0300

    Delete SRC directory

--- SRC/Config/MongoDB/configMongo.js
+++
@@ -1,6 +0,0 @@
-const Mongoose = require("mongoose")
-const Config = require('')
-
-const MongooseConfig = Mongoose.connect(Config.Env.MONGO)
-
-export default MongooseConfig
--- SRC/Middlewares/gzip.js
+++
@@ -1,5 +0,0 @@
-const Compression = require('compression')
-
-const Gzip = app.use(Compression())
-
-export default Gzip
